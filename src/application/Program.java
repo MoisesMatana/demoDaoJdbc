99bc50fce36cd2500ffb681c6e51f783adc360d8
MoisesMatana <musmatan@ucs.br>
2020-07-02 17:27:31 -0300

    Implementação busca seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department department = new Department(1,"Books");
-		System.out.println(department);
-		
-		Seller seller = new Seller(1, "Moises", "moises.matana@gmail.com", new Date(), 800.00, department);
-		
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
+		Seller seller = sellerDao.findById(3);
+
 		System.out.println(seller);
 	}
 }
