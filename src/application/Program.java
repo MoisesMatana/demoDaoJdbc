954a4a6351aad1d5f4539c22bb3e65b57772c1c6
MoisesMatana <musmatan@ucs.br>
2020-07-02 16:00:52 -0300

    Classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department department = new Department(1,"Books");
 		System.out.println(department);
+		
+		Seller seller = new Seller(1, "Moises", "moises.matana@gmail.com", new Date(), 800.00, department);
+		System.out.println(seller);
 	}
 }
