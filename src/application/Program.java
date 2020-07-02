f4400f04e3f967330358805881880c6400018211
MoisesMatana <musmatan@ucs.br>
2020-07-02 16:45:58 -0300

    Classes DaoFactory e SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,9 @@ public class Program {
 		System.out.println(department);
 		
 		Seller seller = new Seller(1, "Moises", "moises.matana@gmail.com", new Date(), 800.00, department);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 	}
 }
